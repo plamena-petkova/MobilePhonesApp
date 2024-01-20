132e93c0f353a5001281d9e5b525fd52490b0cb9
Plamena Petkova <pl.petkova@gmail.com>
2024-01-20 12:27:53 +0200

    Changes

--- rest/index.js
+++ rest/index.js
@@ -56,23 +56,7 @@ async function start() {
         process.exit(1);
     }
 
-    // --------------------------deployment------------------------------
-
-const __dirname1 = path.resolve();
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "app/dist/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "app", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
-
-// --------------------------deployment------------------------------
+  
 
 
 }
